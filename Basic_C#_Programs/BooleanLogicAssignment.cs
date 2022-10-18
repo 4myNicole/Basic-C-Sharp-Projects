840df1416c9f39018da8cdf293e811198df96aa6
4myNicole <91946031+4myNicole@users.noreply.github.com>
2022-10-18 17:13:33 +0100

    Add files via upload

---
+++ Basic_C#_Programs/BooleanLogicAssignment.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+        static void Main()
+	{
+
+        Console.WriteLine("What is your age?");
+        int yourAge = Console.ReadLine();
+
+        Console.WriteLine("Have you ever had a DUI violation?");
+        duiViolation = Console.ReadLine();
+
+        Console.WriteLine("How many speeding tickets have you had?");
+        int speedingTickets = Console.ReadLine();
+
+        
+
+    	}
+}
