50a5bbe08017929b3fc35783cdc00df2a8e7afe4
4myNicole <91946031+4myNicole@users.noreply.github.com>
2022-10-19 10:52:21 +0100

    Update IncomeComparisonAssignment.cs

--- Basic_C#_Programs/IncomeComparisonAssignment.cs
+++ Basic_C#_Programs/IncomeComparisonAssignment.cs
@@ -14,27 +14,28 @@ class Program
         Console.WriteLine("Hello Person 1");
 
         Console.WriteLine("What is your hourly rate?");
-        decimal hourlyRateP1 = Console.ReadLine();
+        decimal hourlyRateP1 = Convert.ToDecimal(Console.ReadLine());
 
         Console.WriteLine("How many hours do you work a week?");
-        decimal weeklyHoursP1 = Console.ReadLine();
+        decimal weeklyHoursP1 =  Convert.ToDecimal(Console.ReadLine());
 
          Console.WriteLine("Hello Person 2");
 
         Console.WriteLine("What is your hourly rate?");
-        decimal hourlyRateP2 = Console.ReadLine();
+        decimal hourlyRateP2 =  Convert.ToDecimal(Console.ReadLine());
 
         Console.WriteLine("How many hours do you work a week?");
-        decimal weeklyHoursP2 = Console.ReadLine();
+        decimal weeklyHoursP2 =  Convert.ToDecimal(Console.ReadLine());
 
-        int p1AnnualSalary = (hourlyRateP1 * weeklyHoursP1) * 52
-        int p2AnnualSalary = (hourlyRateP2 * weeklyHoursP2) * 52
+        decimal p1AnnualSalary = (hourlyRateP1 * weeklyHoursP1) * 52;
+        decimal p2AnnualSalary = (hourlyRateP2 * weeklyHoursP2) * 52;
         Console.WriteLine("Person 1\'s annual salary: \n" + p1AnnualSalary);
         Console.WriteLine("Person 2\'s annual salary: \n" + p2AnnualSalary);
 
         Console.WriteLine("Person 1 makes more money than person 2");
         bool GreaterThanPerson1 = p1AnnualSalary > p2AnnualSalary;
 		Console.WriteLine(GreaterThanPerson1);
+						  
 
 	}
 }
