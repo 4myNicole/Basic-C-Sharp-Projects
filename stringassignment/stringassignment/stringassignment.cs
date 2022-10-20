bb07e3f11800228a528640d94334efce7b911c0e
4myNicole <91946031+4myNicole@users.noreply.github.com>
2022-10-20 16:50:07 +0100

    Add files via upload

---
+++ stringassignment/stringassignment/stringassignment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace stringassignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cstring one = "Hello! ";
+            string two = "My name is Amy. ";
+            string three = "Nice to meet you.";
+            string greeting = (one + two + three);
+
+            one = one.ToUpper();
+            Console.WriteLine(greeting);
+            Console.WriteLine(one);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Hola ");
+            sb.Append("Me llamo Amy, ");
+            sb.Append("Mucho Gusto!");
+
+            Console.WriteLine(sb);
+        }
+    }
+}
