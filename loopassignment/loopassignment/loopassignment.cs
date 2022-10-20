c36e7456cd21ce68e5c93873fb9516cc47ab4a17
4myNicole <91946031+4myNicole@users.noreply.github.com>
2022-10-20 16:51:33 +0100

    Add files via upload

---
+++ loopassignment/loopassignment/loopassignment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace loopassignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 50;
+            while (i <= 100)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+
+            int j = 99;
+            do
+            {
+                Console.WriteLine(j);
+                j--;
+            }
+
+            while (j >= 50);
+        }
+    }
+}
