cf3d5c36a41d14d3a43326b14693864d57f51860
4myNicole <91946031+4myNicole@users.noreply.github.com>
2022-10-20 17:01:27 +0100

    Add files via upload

---
+++ dailyreportassignment/dailyreportassignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dailyreportassignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
