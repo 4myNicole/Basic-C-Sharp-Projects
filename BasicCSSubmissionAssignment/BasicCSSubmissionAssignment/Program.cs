4635d8204156f869f6e68675fe6ae9f985008f63
4myNicole <91946031+4myNicole@users.noreply.github.com>
2022-10-20 16:35:22 +0100

    Add files via upload

---
+++ BasicCSSubmissionAssignment/BasicCSSubmissionAssignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicCSSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int yourAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI violation?");
+            string duiViolation = Console.ReadLine();
+            bool yesOrNo = duiViolation == "yes";
+            Console.WriteLine(yesOrNo);
+
+            Console.WriteLine("How many speeding tickets have you had?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            bool qualified = yourAge > 15 && duiViolation == false && speedingTickets <= 3;
+        }
+    }
+}
