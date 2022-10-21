4f561c2cb618939312ea0cdba412797f06bdb65f
4myNicole <91946031+4myNicole@users.noreply.github.com>
2022-10-21 17:51:45 +0100

    Update booleanassignment.cs

--- BooleanLogicAssignment/BooleanLogicAssignment/booleanassignment.cs
+++ BooleanLogicAssignment/BooleanLogicAssignment/booleanassignment.cs
@@ -9,10 +9,8 @@ namespace BooleanLogicAssignment
             Console.WriteLine("What is your age?");
             int yourAge = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Have you ever had a DUI violation?");
-            string duiViolation = Console.ReadLine();
-            bool yesOrNo = duiViolation == "yes";
-            Console.WriteLine(yesOrNo);
+            Console.WriteLine("Have you ever had a DUI violation? (please answer true or false)");
+            bool duiViolation = Convert.ToBoolean(Console.ReadLine());
 
             Console.WriteLine("How many speeding tickets have you had?");
             int speedingTickets = Convert.ToInt32(Console.ReadLine());
